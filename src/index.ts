798a6a0a69326a9dc63596fb91ab4e4835a002e6
Eugene Doronin <e.doronin@velvetech.dev>
2023-03-26 12:19:49 +0700

    feat: expose query model

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SqliteConnection } from './sqlite-connection.js'
 export { ExpoSqliteProvider } from './sqlite-provider.js'
 export { SqliteQueryable, SqliteRepository } from './sqlite-repository.js'
+export { QueryModel, WhereModel, WhereOperation } from './query-model.js'
