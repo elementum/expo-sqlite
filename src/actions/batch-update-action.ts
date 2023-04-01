4dcbe2a7153141f1e22132c4d69af7c40d3ff04a
Eugene Doronin <e.doronin@velvetech.dev>
2023-04-01 22:53:56 +0700

    feat!: make setter type safe

--- src/actions/batch-update-action.ts
+++ src/actions/batch-update-action.ts
@@ -1,15 +1,16 @@
 import { QueryModel } from '@elementum/expo-sqlite'
 import { Client } from '../client.js'
 import { SqliteAction } from './sqlite-action.js'
+import { getValue } from '../value-factory.js'
 
 export class BatchUpdateAction<T = any> extends SqliteAction<T> {
     constructor(client: Client, private query: Partial<QueryModel>, private table: string) {
         super(client)
     }
 
-    private setters: Record<string, string> = {}
+    private setters: Record<string, any> = {}
 
-    set<TKey extends keyof T>(key: TKey, value: string) {
+    set<TKey extends keyof T>(key: TKey, value: T[TKey]) {
         this.setters[key as string] = value
         return this
     }
@@ -17,7 +18,7 @@ export class BatchUpdateAction<T = any> extends SqliteAction<T> {
     async invoke() {
         let sql = `UPDATE ${this.table} SET `
         sql += Object.entries(this.setters)
-            .map((e) => `${e[0]} = ${e[1]}`)
+            .map((e) => `${e[0]} = ${getValue(e[1])}`)
             .join(', ')
 
         if (this.query.where) {
